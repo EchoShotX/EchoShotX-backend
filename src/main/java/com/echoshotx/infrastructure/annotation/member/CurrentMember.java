2ab5c1d47ad8efe77573b3192a7ce23f3917e386
hamlsy <dltmddud1122@naver.com>
2025-06-20 16:00:19 +0900

    feat: add CurrentMember annotation #5

---
+++ src/main/java/com/echoshotx/infrastructure/annotation/member/CurrentMember.java
@@ -0,0 +1,11 @@
+package com.echoshotx.infrastructure.annotation.member;
+import io.swagger.v3.oas.annotations.Parameter;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Parameter(hidden = true)
+public @interface CurrentMember {
+}
